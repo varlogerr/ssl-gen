6706cbfcf80e50badc53801856f85ad6151ea0c2
Varlog Err <varlog.err@gmail.com>
2021-10-26 00:31:28 +0300

    improove hook.bash

--- hook.bash
+++ hook.bash
@@ -1,8 +1,10 @@
 export SSL_GEN_DIR="${SSL_GEN_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}"
 
-if \
-    ! grep -qFo "${SSL_GEN_DIR}:" <<< "${PATH}" \
-    && ! grep -qFo ":${SSL_GEN_DIR}" <<< "${PATH}" \
-; then
-  export PATH="${PATH:+${PATH}:}${SSL_GEN_DIR}"
-fi
+export PATH="$(
+  if tr ':' '\n' <<< "${PATH}" | grep -Fx "${SSL_GEN_DIR}"; then
+    # ${SSL_GEN_DIR} is in the ${PATH}
+    echo "${PATH}"
+  else
+    echo "${SSL_GEN_DIR}:${PATH}"
+  fi
+)"
