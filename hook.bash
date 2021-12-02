c6658e7d3b383f5c1946d2eb8a32dfe99de00e18
Varlog Err <varlog.err@gmail.com>
2021-12-02 12:16:52 +0200

    minor changes to hook.bash

--- hook.bash
+++ hook.bash
@@ -1,7 +1,9 @@
 export SSL_GEN_BINDIR="${SSL_GEN_BINDIR:-$(dirname "$(realpath "${BASH_SOURCE[0]}")")/bin}"
 
-if !  tr ':' '\n' <<< "${PATH}" | sort | uniq \
-      | grep -Fxq "${SSL_GEN_BINDIR}"; then
+if \
+  !  tr ':' '\n' <<< "${PATH}" | sort | uniq \
+  | grep -Fxq "${SSL_GEN_BINDIR}" \
+; then
   # ${SSL_GEN_BINDIR} is not in the ${PATH}
   export PATH="${SSL_GEN_BINDIR}:${PATH}"
 fi
