b592738b3dc77ad9f96ccf176cfb247dd586afc9
Varlog Err <varlog.err@gmail.com>
2021-12-03 11:32:42 +0200

    make add to path prependable

--- hook.bash
+++ hook.bash
@@ -1,9 +1,17 @@
 export SSL_GEN_BINDIR="${SSL_GEN_BINDIR:-$(dirname "$(realpath "${BASH_SOURCE[0]}")")/bin}"
+export SSL_GEN_PREPEND_PATH="${SSL_GEN_PREPEND_PATH:-0}"
 
 if \
   !  tr ':' '\n' <<< "${PATH}" | sort | uniq \
   | grep -Fxq "${SSL_GEN_BINDIR}" \
 ; then
   # ${SSL_GEN_BINDIR} is not in the ${PATH}
-  export PATH="${SSL_GEN_BINDIR}:${PATH}"
+
+  if [[ ${SSL_GEN_PREPEND_PATH} == 1 ]]; then
+    # prepend ssl-gen bin path to $PATH
+    export PATH="${SSL_GEN_BINDIR}${PATH:+:${PATH}}"
+  else
+    # append ssl-gen bin path to $PATH
+    export PATH="${PATH:+${PATH}:}${SSL_GEN_BINDIR}"
+  fi
 fi
